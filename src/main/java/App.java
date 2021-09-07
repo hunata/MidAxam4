2c83249f99d3b5c51d378c2e36ba1d147ba8898c
hunata <hunata87@gmail.com>
2021-09-07 20:45:58 +0700

    Inprogress 2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,16 @@
 import entities.Passenger;
-import entities.Ticket;
 
 public class App {
     public static void main(String[] args) {
-        Passenger ps = new Passenger();
-        ps.inputPassengerInfo();
-        System.out.println(ps);
-//        Ticket tk = new Ticket();
-//        tk.inputTicketInfo();
-//        System.out.println(tk);
+        // Nhập vào thông tin hành khách và thông tin vé
+        Passenger[] passengers = Passenger.inputPassengerList();
+
+        // In ra thông tin hành khách số lượng vé và tổng tiền
+        Passenger.outputPassengerList(passengers);
+
+        //Sắp xếp danh sách hành khách theo chiều giảm dần của Tổng tiền.
+        // In ra danh sách
+        Passenger.sortPassenger(passengers);
 
     }
 }
--- src/main/java/entities/Passenger.java
+++ src/main/java/entities/Passenger.java
@@ -10,13 +10,14 @@ public class Passenger {
     private Ticket[] danhSachVe;
 
     @Override
-    public String toString() {
+    public String toString( ) {
         final StringBuffer sb = new StringBuffer("Thông tin hành khách:\n");
         sb.append("Họ tên: ").append(hoTen).append('\n');
         sb.append("Gioi tính: ").append(gioiTinh).append('\n');
-        sb.append("Tuổi:").append(tuoi);
-        sb.append("\nDanh sách vé: \n").append(danhSachVe == null ? "null" : Arrays.asList(danhSachVe).toString());
-//        sb.append('}');
+        sb.append("Tuổi:").append(tuoi).append('\n');
+        for (Ticket ticket: danhSachVe) {
+            sb.append(ticket.toString());
+        }
         return sb.toString();
     }
 
@@ -30,7 +31,7 @@ public class Passenger {
     public Passenger() {
     }
 
-    public void inputPassengerInfo() {
+    public Passenger inputPassengerInfo() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Nhập vào Họ Tên: ");
         this.hoTen = sc.nextLine();
@@ -45,5 +46,54 @@ public class Passenger {
             System.out.print("Vé " + (i+1) + ": \n");
             danhSachVe[i] = new Ticket().inputTicketInfo();
         }
+        return this;
+    }
+
+    public static Passenger[] inputPassengerList(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhập vào số lượng hành khách: ");
+        int n = sc.nextInt();
+        Passenger[] ps = new Passenger[n];
+        for (int i = 0; i<n;i++){
+            System.out.println("Hành khách " + (i+1)+ ":\n");
+            ps[i] = new Passenger().inputPassengerInfo();
+        }
+        return ps;
+    }
+
+    public static void outputPassengerList(Passenger[] passengers) {
+//        for (Passenger passenger : passengers) {
+//            System.out.println(passenger.toString());
+//        }
+        for (int i = 0; i < passengers.length; i++){
+            System.out.println(passengers[i].toString());
+        System.out.println("Tổng tiền: "+ passengers[i].totalPrice());
+        }
+    }
+
+    public int totalPrice(){
+        int total=0;
+        Passenger ps = new Passenger();
+        for (int i=0; i<this.danhSachVe.length; i++){
+            total += danhSachVe[i].getGiaVe();
+        }
+        return total;
+    }
+    public static void sortPassenger(Passenger[] psList) {
+        for (int i = 0; i < psList.length - 1; i++) {
+            for (int j = i + 1; j < psList.length; j++) {
+                if (psList[j].totalPrice() > psList[i].totalPrice()) {
+                    Passenger tmp = psList[i];
+                    psList[i] = psList[j];
+                    psList[j] = tmp;
+                }
+            }
+        }
+
+        for (int i = 0; i < psList.length; i++) {
+            System.out.println("Hành khách sau khi sắp xếp " + (i + 1));
+            System.out.println(psList[i].toString());
+        }
     }
 }
+
